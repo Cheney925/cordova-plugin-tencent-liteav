c2449f2156070b2a92ed59c1391192c034c0d9b8
Cheney Hu <cheney925@gmail.com>
2018-01-17 16:21:16 +0800

    修复iOS播放不显示的问题

--- src/ios/CLiteAV.m
+++ src/ios/CLiteAV.m
@@ -22,23 +22,48 @@
     if (self.videoView) return;
     self.videoView = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     [self.webView.superview addSubview:self.videoView];
-    [self.webView.superview bringSubviewToFront:self.webView];
+    [self.webView.superview bringSubviewToFront:self.videoView];
 }
 
 - (void) destroyVideoView {
     if (!self.videoView) return;
     [self.videoView removeFromSuperview];
     self.videoView = nil;
-    // 把 webView 变回白色
-    // [self.webView setBackgroundColor:[UIColor whiteColor]];
 }
 
 - (void) startPlay:(CDVInvokedUrlCommand*)command {
     if (self.livePlayer) return;
     
     NSString* url = [command.arguments objectAtIndex:0];
-    NSInteger playType = (NSInteger)[command.arguments objectAtIndex:1];
-
+    NSInteger type = (NSInteger)[command.arguments objectAtIndex:1];
+    TX_Enum_PlayType playType;
+    switch (type) {
+        case 0:
+            playType = PLAY_TYPE_LIVE_RTMP;
+            break;
+        case 1:
+            playType = PLAY_TYPE_LIVE_FLV;
+            break;
+        case 2:
+            playType = PLAY_TYPE_VOD_FLV;
+            break;
+        case 3:
+            playType = PLAY_TYPE_VOD_HLS;
+            break;
+        case 4:
+            playType = PLAY_TYPE_VOD_MP4;
+            break;
+        case 5:
+            playType = PLAY_TYPE_LIVE_RTMP_ACC;
+            break;
+        case 6:
+            playType = PLAY_TYPE_LOCAL_VIDEO;
+            break;
+        default:
+            playType = PLAY_TYPE_LIVE_RTMP;
+            break;
+    }
+    
     [self prepareVideoView];
 
     self.livePlayer = [[TXLivePlayer alloc] init];
